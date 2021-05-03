5520b106d9af14441c5796d480508b77bf00d607
ujjwal kumar <ujjwal@ujjwals-MacBook-Air.local>
2021-05-03 18:09:29 +0530

    Blog-site deploy modify

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json({limit: '30mb',extended:false}));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV == "production") {
   app.use(express.static(path.join(__dirname, "/frontend/build")));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
