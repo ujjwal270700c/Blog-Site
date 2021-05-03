d42d9f0df03b71689fbf8611a31ad126874ca143
ujjwal kumar <ujjwal@ujjwals-MacBook-Air.local>
2021-05-03 18:31:41 +0530

    Blog-site deploy modify

--- frontend/src/auth/AuthState.js
+++ frontend/src/auth/AuthState.js
@@ -55,7 +55,7 @@ const AuthState = (props) => {
         }
         console.log(formData);
         try {
-            const res=await axios.post('/api/users',formData,config);
+            const res=await axios.post('https://blog-app334.herokuapp.com/api/users',formData,config);
             console.log(res);
             dispatch({
                 type:REGISTER_USER,
@@ -77,7 +77,7 @@ const AuthState = (props) => {
             }
         }
         try {
-            const res=await axios.post('http://localhost:5002/api/auth',formData,config);
+            const res=await axios.post('https://blog-app334.herokuapp.com/api/auth',formData,config);
             dispatch({
                 type:LOGIN_USER,
                 payload:res.data
